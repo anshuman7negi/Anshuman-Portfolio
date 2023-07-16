744386c266e6cb36334ec24dde67261c1c75c58c
anshuman7negi <anshumannegi777@gmail.com>
2023-07-16 16:57:39 +0530

    added javascript to dynamically add projects

---
+++ index.js
@@ -0,0 +1,59 @@
+const projectData = [
+    {
+        projectImage: "./ajju.jpg",
+        projectTitle: "To Do List",
+        languageUsed: ["HTML","CSS","JavaScript",]
+    },
+    {
+        projectImage: './ajju.jpg',
+        projectTitle: "To Do List",
+        languageUsed: ["HTML","CSS","JavaScript",]
+    },
+    {
+        projectImage: 'ajju.jpg',
+        projectTitle: "To Do List",
+        languageUsed: ["HTML","CSS","JavaScript",]
+    }
+]
+
+const project = document.querySelector('.projects');
+
+for(let i = 0;i < projectData.length; i +=1) {
+    let languages = '';
+  for (let j = 0; j < projectData[i].languageUsed.length; j++) {
+    languages += `<li>${projectData[i].languageUsed[j]}</li>`;
+  }
+    project.innerHTML += `<div class="card">
+    <img src="${projectData[i].projectImage}" alt="project Image" />
+    <div class="cardDetails">
+      <p>${projectData[i].projectTitle}</p>
+      <ul>
+        ${languages}
+      </ul>
+      <button>See Project</button>
+    </div>
+  </div>`
+}
+
+const skillsData = {
+  languages: ["HTML", "CSS", "JavaScript"],
+  frameworks: ["React.js", "Ruby on Rails", "RSpec", "Capybara", "Selenium"],
+  skills: ["Database Management", "Version Control", "CLI", "Web Development", "API Design"]
+};
+
+const skillContainers = document.querySelectorAll('.skills');
+
+for (let i = 0; i < skillContainers.length; i++) {
+  const skillType = skillContainers[i].id;
+  const skills = skillsData[skillType];
+
+  if (skills) {
+    skills.forEach((skill) => {
+      const skillItem = document.createElement('li');
+      skillItem.textContent = skill;
+      skillContainers[i].appendChild(skillItem);
+    });
+  }
+}
+
+  
