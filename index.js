b39eb45cd993229b6d830a939f7a2a40bf413f6b
anshuman7negi <anshumannegi777@gmail.com>
2023-07-16 17:24:45 +0530

    mobile version finished

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const projectData = [
-    {
-        projectImage: "./ajju.jpg",
-        projectTitle: "To Do List",
-        languageUsed: ["HTML","CSS","JavaScript",]
-    },
-    {
-        projectImage: './ajju.jpg',
-        projectTitle: "To Do List",
-        languageUsed: ["HTML","CSS","JavaScript",]
-    },
-    {
-        projectImage: 'ajju.jpg',
-        projectTitle: "To Do List",
-        languageUsed: ["HTML","CSS","JavaScript",]
-    }
-]
+  {
+    projectImage: './ajju.jpg',
+    projectTitle: 'To Do List',
+    languageUsed: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    projectImage: './ajju.jpg',
+    projectTitle: 'To Do List',
+    languageUsed: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    projectImage: 'ajju.jpg',
+    projectTitle: 'To Do List',
+    languageUsed: ['HTML', 'CSS', 'JavaScript'],
+  },
+];
 
 const project = document.querySelector('.projects');
 
-for(let i = 0;i < projectData.length; i +=1) {
-    let languages = '';
-  for (let j = 0; j < projectData[i].languageUsed.length; j++) {
+for (let i = 0; i < projectData.length; i += 1) {
+  let languages = '';
+  for (let j = 0; j < projectData[i].languageUsed.length; j += 1) {
     languages += `<li>${projectData[i].languageUsed[j]}</li>`;
   }
-    project.innerHTML += `<div class="card">
+  project.innerHTML += `<div class="card">
     <img src="${projectData[i].projectImage}" alt="project Image" />
     <div class="cardDetails">
       <p>${projectData[i].projectTitle}</p>
@@ -32,18 +32,18 @@ for(let i = 0;i < projectData.length; i +=1) {
       </ul>
       <button>See Project</button>
     </div>
-  </div>`
+  </div>`;
 }
 
 const skillsData = {
-  languages: ["HTML", "CSS", "JavaScript"],
-  frameworks: ["React.js", "Ruby on Rails", "RSpec", "Capybara", "Selenium"],
-  skills: ["Database Management", "Version Control", "CLI", "Web Development", "API Design"]
+  languages: ['HTML', 'CSS', 'JavaScript'],
+  frameworks: ['React.js', 'Ruby on Rails', 'RSpec', 'Capybara', 'Selenium'],
+  skills: ['Database Management', 'Version Control', 'CLI', 'Web Development', 'API Design'],
 };
 
 const skillContainers = document.querySelectorAll('.skills');
 
-for (let i = 0; i < skillContainers.length; i++) {
+for (let i = 0; i < skillContainers.length; i += 1) {
   const skillType = skillContainers[i].id;
   const skills = skillsData[skillType];
 
@@ -55,5 +55,3 @@ for (let i = 0; i < skillContainers.length; i++) {
     });
   }
 }
-
-  
