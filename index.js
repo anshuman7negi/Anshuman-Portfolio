9fdb304b3158ea688d72006fe03aecf9590e9fec
anshuman7negi <anshumannegi777@gmail.com>
2023-07-29 15:05:16 +0530

    Added popup card dynamically

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const projectData = [
   },
   {
     projectImage: 'ajju.jpg',
-    projectTitle: 'To Do List',
+    projectTitle: 'To Do app',
     languageUsed: ['HTML', 'CSS', 'JavaScript'],
   },
 ];
@@ -30,9 +30,45 @@ for (let i = 0; i < projectData.length; i += 1) {
       <ul>
         ${languages}
       </ul>
-      <button>See Project</button>
+      <button class="seeProjectBtn">See Project</button>
     </div>
   </div>`;
+}
+const body = document.querySelector('body');
+for (let i = 0; i < projectData.length; i += 1) {
+  let languages = '';
+  for (let j = 0; j < projectData[i].languageUsed.length; j += 1) {
+    languages += `<li>${projectData[i].languageUsed[j]}</li>`;
+  }
+  body.innerHTML += ` <section id="card-${i}" class="seeProject">
+  <div class="seeProject-container">
+  <div class="cancelBtn-container">
+  <img class="cancel-button" src="./images/mobileImages/card-cancel-btn.svg" alt="">
+  </div>
+  <div class="top-container">
+    <img src="${projectData[i].projectImage}" alt="project image">
+  </div>
+  <div class="middle-container">
+    <h3>${projectData[i].projectTitle}</h3>
+    <ul>
+        ${languages}
+      </ul>
+  </div>
+  <div class="bottom-container">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it 1960s with the
+        releaLorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+       when an unknown printer took a galley of type and scrambled it 1960s with the relea
+    </p>
+    <div class="project-buttons">
+    <button>See Live</button>
+    <button>See Source</button>
+    </div>
+  </div>
+  </div>
+</section>`;
 }
 
 const skillsData = {
@@ -54,4 +90,19 @@ for (let i = 0; i < skillContainers.length; i += 1) {
       skillContainers[i].appendChild(skillItem);
     });
   }
+}
+const seeProject = document.querySelectorAll('.seeProjectBtn');
+for (let i = 0; i < seeProject.length; i += 1) {
+  seeProject[i].addEventListener('click', () => {
+    document.querySelector('main').style.display = 'none';
+    document.getElementById(`card-${i}`).style.display = 'flex';
+  });
+}
+
+const cancel = document.querySelectorAll('.cancel-button');
+for (let i = 0; i < seeProject.length; i += 1) {
+  cancel[i].addEventListener('click', () => {
+    document.querySelector('main').style.display = 'block';
+    document.getElementById(`card-${i}`).style.display = 'none';
+  });
 }
